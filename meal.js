4e51d41592c45f04b8ef90e10034f692b7f5e090
Vinesh Kannan <vingkan@gmail.com>
2015-09-23 18:43:20 -0500

    Updated meal.toString() with moment.js

--- meal.js
+++ meal.js
@@ -12,14 +12,14 @@ function Meal(snapshot){
 	this.name = snapshot.name;
 	this.id = snapshot.id;
 	this.chef = snapshot.chef;
-	this.timestamp = new Date(snapshot.timestamp);
+	this.timestamp = snapshot.timestamp;
 	this.img = snapshot.img;
 	this.price = snapshot.price;
 	this.capacity = snapshot.capacity;
 }
 
 Meal.prototype.toString = function(){
-	return this.name + " by " + this.chef + " on " + this.timestamp + " for " + this.capacity + " people at " + money(this.price) + " each.";
+	return this.name + " by " + this.chef + ": " + moment(this.timestamp).calendar() + " for " + this.capacity + " people at " + money(this.price) + " each.";
 }
 
 function money(value){
