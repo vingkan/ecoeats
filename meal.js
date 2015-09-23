2a23b275c5fbf42e5ff154b49b2698bcf5099c8a
Vinesh Kannan <vingkan@gmail.com>
2015-09-23 18:12:04 -0500

    Exported meal to client array

--- meal.js
+++ meal.js
@@ -1,3 +1,16 @@
 function Meal(snapshot){
 	this.name = snapshot.name;
+	this.id = snapshot.id;
+	this.chef = snapshot.chef;
+	this.img = snapshot.img;
+	this.price = snapshot.price;
+	this.capacity = snapshot.capacity;
+}
+
+Meal.prototype.toString = function(){
+	return this.name + " by " + this.chef + " for " + this.capacity + " people at " + money(this.price) + " each.";
+}
+
+function money(value){
+	return "$" + value.toFixed(2);
 }
