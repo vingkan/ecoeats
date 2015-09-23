b08255b33df360501341b59458f85f402987f858
Vinesh Kannan <vingkan@gmail.com>
2015-09-23 18:40:10 -0500

    Added ForEach Loop on Meals Database

--- meal.js
+++ meal.js
@@ -1,14 +1,25 @@
+function loadMeals(){
+	var upcomingMeals = document.getElementById('upcomingMeals');
+	if(meals.length > 0){
+		upcomingMeals.innerHTML = "";
+		for(var m = 0; m < meals.length; m++){
+			upcomingMeals.innerHTML += '<p>' + meals[m] + '</p>';
+		}
+	}
+}
+
 function Meal(snapshot){
 	this.name = snapshot.name;
 	this.id = snapshot.id;
 	this.chef = snapshot.chef;
+	this.timestamp = new Date(snapshot.timestamp);
 	this.img = snapshot.img;
 	this.price = snapshot.price;
 	this.capacity = snapshot.capacity;
 }
 
 Meal.prototype.toString = function(){
-	return this.name + " by " + this.chef + " for " + this.capacity + " people at " + money(this.price) + " each.";
+	return this.name + " by " + this.chef + " on " + this.timestamp + " for " + this.capacity + " people at " + money(this.price) + " each.";
 }
 
 function money(value){
