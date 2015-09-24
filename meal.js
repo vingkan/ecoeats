cae6542c3da056016439d8610c2112d07b21484d
Vinesh Kannan <vingkan@gmail.com>
2015-09-24 15:51:56 -0500

    Sliding meal preview boxes

--- meal.js
+++ meal.js
@@ -8,6 +8,7 @@ function loadMeals(){
 		for(var m = 0; m < meals.length; m++){
 			upcomingMeals.innerHTML += meals[m].toHTML();
 		}
+		upcomingMeals.innerHTML += meals[0].getHTMLPayButton();
 	}
 	else{
 		upcomingMeals.innerHTML += '<p>No meals are currently listed. Host your own!</p>';
@@ -32,8 +33,11 @@ Meal.prototype.toHTML = function(){
 	var html = '';
 	html += '<div class="mealPreview" onclick="viewMeal(&#39;' + this.id + '&#39;)" style="background-image: url(&#39;style/meals/' + this.img + '&#39;);">';
 		html += '<div class="info">';
-			html += '<div class="bubble price">$' + this.price + '</div>';
-			html += '<div class="bubble capacity">' + this.capacity + '</div>';
+			html += '<div class="mealPay">';
+			html += this.getHTMLPayButton();
+			html += '</div>';
+			html += '<div class="bubble price">$' + this.price + ' per person</div>';
+			html += '<div class="bubble capacity">' + this.capacity + ' spots left </div>';
 			html += '<div class="bubble date">' + moment(this.timestamp).format('MM/DD h:mm A') + '</div>';
 		html += '</div>';
 		html += '<div class="mealTitle">';
@@ -58,6 +62,7 @@ Meal.prototype.getHTMLPayButton = function(){
 	  button += 'data-type="simple"';
 	button += '>';
 	button += '</script>';
+	return button;
 }
 
 function money(value){
